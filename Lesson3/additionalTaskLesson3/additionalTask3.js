7f377579ab15c809056a503f2b884a13d9b8695a
Vlad <tixon16@rambler.ru>
2019-03-05 19:03:40 +0300

    version 3.2

--- Lesson3/additionalTaskLesson3/additionalTask3.js
+++ Lesson3/additionalTaskLesson3/additionalTask3.js
@@ -1,48 +1,52 @@
 'use strict';
 
 // 1)
-// let str = "урок-3-был слишком легким";
-// console.log(str[0].toUpperCase() + str.substr(1));
+let str = "урок-3-был слишком легким";
+let newStr = str[0].toUpperCase() + str.substr(1);
+console.log(newStr);
 
-//2)
-// console.log(str[0].toUpperCase() + str.slice(1,4) + " " + str.slice(5,6) + " " + str.slice(7));
+// 2)
+newStr = newStr.replace("-3-"," 3 ");
+console.log(newStr);
+// newStr = str[0].toUpperCase() + str.slice(1,4) + " " + str.slice(5,6) + " " + str.slice(7);
 
 // 3)
-// console.log( (str[0].toUpperCase() + str.slice(1,4) + " " + str.slice(5,6)
-//      + " " + str.slice(7)).substring(19,23) + "oo" );
+let a = newStr.slice(-6);
+a = a.replace('легким','легкоо');
+console.log(a);
 
-//4)
-// let arr = [20, 33, 'Человек', 2, 3];
-// arr.splice(2,1);
-// console.log(arr);
 
-// let square = (a) => a*a*a;
+// 4)
+let arr = [20, 33, 'Человек', 2, 3];
+arr.splice(2,1);
+console.log(arr);
 
-// let i = 0;
-// while (i < 4) {
-//     arr[i] = square(arr[i]);
-//     i++;
-// }
-// let result = arr.reduce(function(sum, current) {
-//     return sum + current;
-//   });
-// result = Math.sqrt(result);
-// console.log(result);
+let i = 0;
+while (i < 4) {
+    arr[i] = Math.pow(arr[i], 3);
+    i++;
+}
+console.log(arr);
+let result = arr.reduce(function(sum, current) {
+    return sum + current;
+  });
+result = Math.sqrt(result);
+console.log(result);
 
-//5)
+// 5)
 function done(a) {
-    if (a !== "string") {
+    console.log(a);
+    if (isNaN(a) === true && a !== null) {
         console.log(typeof(a));
-        console.log("str")
+        console.log("str");
     } else {
         console.log(typeof(a));
-        alert("Это не строка!")
+        alert("Это не строка!");
     }
     a = a.trim();
-    console.log("п2");
     if (a.length > 49) {
         a = a.slice(0, 49)+ "...";
         console.log(a);
     }
 }
-done("  макfefeowjeofwefojwoefefоамлоифлоимивфмрогфиамифлшагимлшфгирашр");
+done("   hrthrtwhtwhthwhtrthwtrwhrhwrthwrthtrhtwrhwthwtrhwrtwhwrthwthwrthwtrhwtrtwtrth");
